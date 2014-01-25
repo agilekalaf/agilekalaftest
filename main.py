857fe5f3d2e454fe55084c44c8f3f90ad725e3ed
Greg Belanger <agilekalaf@gmail.com>
2014-01-25 08:50:32 -0700

    v.001
    my first GAE Python web app

---
+++ main.py
@@ -0,0 +1,17 @@
+# Copyright 2012 Digital Inspiration
+# http://www.labnol.org/
+
+import os
+from google.appengine.ext.webapp import template
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+	def get(self,q):
+		if q is None:
+			q = 'index.html'
+		path = os.path.join(os.path.dirname(__file__),q)
+		self.response.headers ['Content-Type'] = 'text/html'
+		self.response.out.write(template.render(path,{}))
+
+app = webapp2.WSGIApplication([('/(.*html)?', MainPage)],debug=True)
